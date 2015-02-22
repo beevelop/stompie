2d03ef3c02b9500748ee2fcbb2624f9b2af266f9
Maik Hummel <yo@beevelop.com>
2015-02-22 08:45:30 +0100

    update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,12 @@
 module.exports = function (grunt) {
 	grunt.initConfig({
-	  uglify: {
-	    my_target: {
-	      files: {
-	        'stompie.min.js': ['stompie.js']
-	      }
-	    }
-	  }
+		uglify: {
+			main: {
+				files: {
+					'stompie.min.js': ['stompie.js']
+				}
+			}
+		}
  	});
 
  	grunt.loadNpmTasks('grunt-contrib-uglify');
